abb15e93beb2574bead098cbdeb7101620457ac5
Ratshitimba Murendwa <murendwaratshitimba@gmail.com>
2022-09-10 15:21:00 +0200

    C++ Hackerrank problems

---
+++ C++/Hello World/solution/Hello.cpp
@@ -0,0 +1,18 @@
+/**
+ * Hackerrank problem #1: Hello world
+ * 
+ * */
+
+#include <iostream>
+using namespace std;
+
+/**
+ * Main method
+ **/
+int main() {
+
+	// display hello world
+	printf("Hello, World!\n");
+
+	return 0;
+}
---
+++ C++/Input and Output/solution/solution.cpp
@@ -0,0 +1,23 @@
+/**
+ * Hackerrank problem #2: Input and Output 
+ **/
+
+#include <iostream>
+using namespace std;
+
+/**
+ * main method
+ **/
+int main() {
+
+	int num1;
+	int num2;
+	int num3;
+
+	// Get three integers from the user
+	cin >> num1 >> num2 >> num3;
+
+	// display their sum
+	cout << num1 + num2 + num3 << endl;
+	return 0;
+}
